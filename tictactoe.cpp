32847d66cc6b0052fc26736bd7956f57bb251084
Lakshit Goyal <lakshitgoyal111@gmail.com>
2024-10-17 23:47:41 +0530

    Add files via upload

---
+++ tictactoe.cpp
@@ -0,0 +1,81 @@
+#include <iostream>
+using namespace std;
+
+char board[3][3] = { {'1', '2', '3'}, {'4', '5', '6'}, {'7', '8', '9'} };
+char currentPlayer = 'X';
+
+void displayBoard() {
+    cout << "-------------" << endl;
+    for (int i = 0; i < 3; i++) {
+        cout << "| ";
+        for (int j = 0; j < 3; j++) {
+            cout << board[i][j] << " | ";
+        }
+        cout << endl << "-------------" << endl;
+    }
+}
+
+bool isWinner() {
+    // Check rows and columns
+    for (int i = 0; i < 3; i++) {
+        if (board[i][0] == board[i][1] && board[i][1] == board[i][2]) return true;
+        if (board[0][i] == board[1][i] && board[1][i] == board[2][i]) return true;
+    }
+    // Check diagonals
+    if (board[0][0] == board[1][1] && board[1][1] == board[2][2]) return true;
+    if (board[0][2] == board[1][1] && board[1][1] == board[2][0]) return true;
+    return false;
+}
+
+bool isDraw() {
+    for (int i = 0; i < 3; i++) {
+        for (int j = 0; j < 3; j++) {
+            if (board[i][j] != 'X' && board[i][j] != 'O') {
+                return false;
+            }
+        }
+    }
+    return true;
+}
+
+void switchPlayer() {
+    currentPlayer = (currentPlayer == 'X') ? 'O' : 'X';
+}
+
+void makeMove() {
+    int move;
+    cout << "Player " << currentPlayer << ", enter your move (1-9): ";
+    cin >> move;
+
+    int row = (move - 1) / 3;
+    int col = (move - 1) % 3;
+
+    if (move < 1 || move > 9 || board[row][col] == 'X' || board[row][col] == 'O') {
+        cout << "Invalid move. Try again." << endl;
+        makeMove();
+    } else {
+        board[row][col] = currentPlayer;
+    }
+}
+
+int main() {
+    cout << "Welcome to Tic Tac Toe!" << endl;
+
+    while (true) {
+        displayBoard();
+        makeMove();
+        if (isWinner()) {
+            displayBoard();
+            cout << "Player " << currentPlayer << " wins!" << endl;
+            break;
+        }
+        if (isDraw()) {
+            displayBoard();
+            cout << "It's a draw!" << endl;
+            break;
+        }
+        switchPlayer();
+    }
+
+    return 0;
+}
